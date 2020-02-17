947dbd735e3b4498aec6f50beda9a01b8feee98a
Justin Stephens <jstephens2020@outlook.com>
2020-02-17 10:45:57 -0500

    Added new Note Block control script
    Now allows multiple AudioSources per note, and uses velocity based volume modulation.

---
+++ Assets/MarimbaNote.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MarimbaNote : MonoBehaviour
+{
+    public static Dictionary<string, MarimbaNote> notes;
+
+    public AudioClip soundToPlay;
+    public bool useVelocityBasedVolume = true;
+    public float velocityVolumeMultiplier = 5.0f;
+
+
+    void Start()
+    {
+        // Add this note block to the global list of notes
+        notes.Add(this.gameObject.name, this);
+    }
+
+
+    // This runs every time the block is hit
+    void OnTriggerEnter(Collider col)
+    {
+        // Audio source to play from
+        AudioSource audio = null;
+
+        // Look through existing sources for an idle AudioSource
+        AudioSource[] sources = this.GetComponents<AudioSource>();
+        foreach (AudioSource a in sources) {
+            if (!a.isPlaying) {
+                audio = a;
+                break;
+            }
+        }
+
+        // If no idle source can be found, create one
+        if (audio == null) {
+            audio = this.gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+        }
+
+        // Set properties of AudioSource, then play clip
+        audio.clip = soundToPlay;
+        audio.volume = (useVelocityBasedVolume) ? col.attachedRigidbody.velocity.magnitude * velocityVolumeMultiplier : 1.0f;
+        audio.Play();
+    }
+}
