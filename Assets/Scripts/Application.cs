b7c580978199b91e8416c9b5d50b16e410e74d1a
Justin Stephens <jstephens2020@outlook.com>
2020-02-17 23:27:35 -0500

    Added sprites for music note rendering. Laid groundwork for application level code.

---
+++ Assets/Scripts/Application.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Application : MonoBehaviour
+{
+    enum State {
+        MAIN,
+        NOTE_DRILL,
+    };
+    static State state = State.MAIN;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
